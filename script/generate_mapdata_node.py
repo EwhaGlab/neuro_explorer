d8e900a93f8f9b63e59e77064a966be033c62f14
Kyungmin Han <kyungmin.hahn@gmail.com>
2023-06-21 13:22:43 +0900

    on dev

--- script/generate_mapdata_node.py
+++ script/generate_mapdata_node.py
@@ -6,8 +6,12 @@ import os
 import random
 import time
 import numpy as np
+import tf2_ros
 import tf
 from gazebo_msgs.srv import DeleteModel, SpawnModel
+from gazebo_msgs.msg import ModelState
+from gazebo_msgs.srv import SetModelState
+from std_srvs.srv import Empty
 from geometry_msgs.msg import *
 from nav_msgs.msg import OccupancyGrid
 from nav_msgs.msg import Odometry
@@ -15,6 +19,7 @@ import pdb
 from scipy.io import savemat
 import cv2
 
+MAX_NUM_DATA = 1000
 
 # class metadata():
 #     def __init__(self, gridmap, ):
@@ -25,6 +30,7 @@ import cv2
 def del_model( strModelName ):
     """ Remove the model with 'modelName' from the Gazebo scene """
     # delete_model : gazebo_msgs/DeleteModel
+    rospy.wait_for_service('gazebo/delete_model')
     del_model_prox = rospy.ServiceProxy('gazebo/delete_model', DeleteModel) # Handle to model spawner
     # rospy.wait_for_service('gazebo/delete_model') # Wait for the model loader to be ready
     # FREEZES EITHER WAY
@@ -32,6 +38,7 @@ def del_model( strModelName ):
 
 
 def spawn_model(strModelName, rx, ry):
+    rospy.wait_for_service('gazebo/spawn_urdf_model')
     spawn_model_prox = rospy.ServiceProxy('gazebo/spawn_urdf_model', SpawnModel)
     with open("/home/hankm/python_ws/neuro_frontier_detection/model/turtlebot3_description.urdf") as f:
         robot_urdf = f.read()
@@ -39,6 +46,32 @@ def spawn_model(strModelName, rx, ry):
     rpose = Pose(Point(x=rx, y=ry, z=0), orient)
     spawn_model_prox(strModelName, robot_urdf, "", rpose, "map")
 
+def set_model_state(strModelName, wx, wy):
+    state_msg = ModelState()
+    state_msg.model_name = strModelName
+    state_msg.pose.position.x = wx
+    state_msg.pose.position.y = wy
+    state_msg.pose.position.z = 0.0
+    state_msg.pose.orientation.x = 0
+    state_msg.pose.orientation.y = 0
+    state_msg.pose.orientation.z = 0
+    state_msg.pose.orientation.w = 1
+    rospy.wait_for_service('/gazebo/set_model_state')
+    try:
+        set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+        resp = set_state( state_msg )
+    except rospy.ServiceException:
+        print("Service call failed")
+
+
+def reset_octomap():
+    reset_map = rospy.ServiceProxy('octomap/_server/reset', Empty)
+
+def reset_world():
+    reset_sim = rospy.ServiceProxy('gazebo/reset_world', Empty)
+
+def reset_gazebo():
+    reset_sim = rospy.ServiceProxy('gazebo/reset_simulation', Empty)
 
 def sub2ind(array_shape, rows, cols):
     ind = rows*array_shape[1] + cols
@@ -78,13 +111,13 @@ def identify_free_cells(gridmap):
     #free_cell_loc = np.flipud(free_cell_loc)
     return free_cell_loc
 
-def savemap(imgname, gridmap, dataidx=0):
+def savemap(resdir, gridmap, dataidx=0):
     gmapimg = gridmap2img(gridmap)
     (height, width) = gmapimg.shape
-    #imgname = 'map%05d.pgm' % dataidx
+    imgname = '%s/map%05d.pgm' % (resdir, dataidx)
     cv2.imwrite(imgname, gmapimg)
     free_cell_loc = identify_free_cells(gridmap)
-    fname_frees = 'frees%05d.csv' % dataidx
+    fname_frees = '%s/frees%05d.csv' % (resdir, dataidx)
     np.savetxt(fname_frees, free_cell_loc, delimiter=",")
 
 
@@ -126,15 +159,18 @@ def main(argv):
     # rate.sleep()
 
     dataidx = 0
+    #tfBuffer = tf2_ros.Buffer()
+    #listener = tf2_ros.TransformListener(tfBuffer)
+    listener = tf.TransformListener()
 
     # dummy repositioning ... b/c /odom changes its name to /turtlebot3/odom after the repositioning
-    
-    odommsg = rospy.wait_for_message("/odom", Odometry, timeout=None)
-    (wx_init, wy_init) = (odommsg.pose.pose.position.x, odommsg.pose.pose.position.y)
-    del_model(str('turtlebot3'))
-    time.sleep(0.5)
-    spawn_model(str('turtlebot3'), wx_init, wy_init)
-    time.sleep(0.8)
+
+    # odommsg = rospy.wait_for_message("/odom", Odometry, timeout=None)
+    # (wx_init, wy_init) = (odommsg.pose.pose.position.x, odommsg.pose.pose.position.y)
+    # del_model(str('turtlebot3'))
+    # time.sleep(0.5)
+    # spawn_model(str('turtlebot3'), wx_init, wy_init)
+    # time.sleep(0.8)
     # print("%f %f"%(odommsg.pose.pose.position.x, odommsg.pose.pose.position.y))
     # gmmsg_prev = rospy.wait_for_message("/map", OccupancyGrid, timeout=None)
     # (wx_prev, wy_prev) = (odommsg.pose.pose.position.x, odommsg.pose.pose.position.y)
@@ -146,89 +182,124 @@ def main(argv):
     # save img and meta data of the init map
     #savemetadata(gridmap, dataidx=0, gx_prev=gx_prev, gy_prev=gy_prev, gx_next=gx_prev, gy_next=gy_prev, reward=0)
 
-    while dataidx < 10000:
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown(): #dataidx < 10000:
+        listener = tf.TransformListener()
+        print("generating data: %d" % dataidx)
 
-        for stepidx in range(0, 10):
+    # load gridmap b4 moving the robot
+        print("req /turtlebot3/odom msg\n")
+        # try:
+        #     trans = tfBuffer.lookup_transform('map', 'base_footprint', rospy.Time())
+        # except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+        #     #print('tf exception: lookup %d connection %d extrap %d' % (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException))
+        #     continue
+        #     rate.sleep()
+        t = rospy.Time(0)
+        listener.waitForTransform('map', 'base_link', t, rospy.Duration(0))
+        (trans, rot) = listener.lookupTransform("map", "base_link", t)
 
-            print("generating data: %d" % dataidx)
+        print("got the tf msg b4 moving (%f %f)\n" %  (trans[0], trans[1]) )#(trans.transform.translation.x, trans.transform.translation.y))
+        (wx_prev, wy_prev) = (trans[0], trans[1]) #(trans.transform.translation.x, trans.transform.translation.y)
 
-        # load gridmap b4 moving the robot
-            print("req /turtlebot3/odom msg\n")
-            odommsg = rospy.wait_for_message("/turtlebot3/odom", Odometry, timeout=None)  # wanted to use /map to /base_link tfrom but tf.transform doesn't work here for some reason
-            print("got /turtlebot3/odom msg and req /map msg\n")
-            gmmsg_prev = rospy.wait_for_message("/map", OccupancyGrid, timeout=None)
-            print("got /map msg\n")
-            (wx_prev, wy_prev) = (odommsg.pose.pose.position.x, odommsg.pose.pose.position.y)
-            (gx_prev, gy_prev) = world2grid(gmmsg_prev, wx_prev, wy_prev)
-            gridmap = np.array(gmmsg_prev.data).reshape((gmmsg_prev.info.height, gmmsg_prev.info.width))
-            explored_map = (gridmap != -1).astype(int)
-            previous_explored_area_size = explored_map.sum() * gmmsg_prev.info.resolution * gmmsg_prev.info.resolution
-        ##########################################################################
-        # find a random move position, then move the robot to there.
-        ##########################################################################
-        # identify free region
-            free_cell_loc = identify_free_cells(gridmap)
-            (dummy, num_free_cell) = free_cell_loc.shape
+        # odommsg = rospy.wait_for_message("/odom", Odometry, timeout=None)
+        # (wx_prev, wy_prev) = (odommsg.pose.pose.position.x, odommsg.pose.pose.position.y)
 
-        # draw a random sample from free region
-            sample_idx = random.sample(range(0,num_free_cell-1), 1)[0]
-            print("sample idx/ tot pts (%d / %d) ", sample_idx, num_free_cell-1)
-            gy_next = free_cell_loc[0, sample_idx] # row idx
-            gx_next = free_cell_loc[1, sample_idx] # column idx
+        gmmsg_prev = rospy.wait_for_message("/map", OccupancyGrid, timeout=None)
+        print("got /map msg\n")
 
-            #pdb.set_trace()
-            # remove / respawn robot to the random free cell
-            (wx_next, wy_next) = grid2world(gmmsg_prev, gx_next, gy_next)
+        (gx_prev, gy_prev) = world2grid(gmmsg_prev, wx_prev, wy_prev)
+        gridmap = np.array(gmmsg_prev.data).reshape((gmmsg_prev.info.height, gmmsg_prev.info.width))
+        explored_map = (gridmap != -1).astype(int)
+        previous_explored_area_size = explored_map.sum() * gmmsg_prev.info.resolution * gmmsg_prev.info.resolution
+    ##########################################################################
+    # find a random move position, then move the robot to there.
+    ##########################################################################
+    # identify free region
+        free_cell_loc = identify_free_cells(gridmap)
+        (dummy, num_free_cell) = free_cell_loc.shape
 
-            # save data b4 we make the movement
-            os.chdir(resdir)
-            imgname = 'map_before_moving%05d.pgm' % dataidx
-            savemap(imgname, gridmap, dataidx)
-            fname = 'metadata_before_moving%05d.dat' % dataidx
-            savemetadata(fname, dataidx, gx_prev, gy_prev, gx_next, gy_next, 0)
-            os.chdir(workdir)
+    # draw a random sample from free region
+        sample_idx = random.sample(range(0, num_free_cell-1), 1)[0]
+        print("sample idx/ tot pts (%d / %d) " % (sample_idx, num_free_cell-1))
+        gy_next = free_cell_loc[0, sample_idx] # row idx
+        gx_next = free_cell_loc[1, sample_idx] # column idx
 
-        # re-positioning the robot
-            del_model(str('turtlebot3'))
-            time.sleep(0.5)
-            spawn_model(str('turtlebot3'), wx_next, wy_next)
-            time.sleep(0.8)
+        #pdb.set_trace()
+        # remove / respawn robot to the random free cell
+        (wx_next, wy_next) = grid2world(gmmsg_prev, gx_next, gy_next)
 
-        # load new map w/ updated/new scanned region
-            print("req /map msg\n")
-            gmmsg_next = rospy.wait_for_message("/map", OccupancyGrid, timeout=None)
-            print("got /map msg\n")
-            gridmap = np.array(gmmsg_next.data).reshape((gmmsg_next.info.height, gmmsg_next.info.width))
+        # save data b4 we make the movement
+        #imgname = '%s/map_before_moving%05d.pgm' % (resdir, dataidx)
+        savemap(resdir, gridmap, dataidx)
+        fname = '%s/metadata_before_moving%05d.dat' % (resdir, dataidx)
+        savemetadata(fname, dataidx, gx_prev, gy_prev, gx_next, gy_next, 0)
+        print("finished saving data b4 moving the robot \n")
+    # re-positioning the robot
+        del_model(str('turtlebot3'))
+        print("robot removed \t")
+        time.sleep(0.5)
+        spawn_model(str('turtlebot3'), wx_next, wy_next)
+        print("then it is respawned \n")
+        #set_model_state('turtlebot3', wx_next, wy_next)
+        time.sleep(0.5)
 
-            explored_map = (gridmap != -1).astype(int)
-            explored_area_size = explored_map.sum() * gmmsg_next.info.resolution * gmmsg_next.info.resolution
-            reward = explored_area_size - previous_explored_area_size
-            reward = max(reward, 0) # This wont happen but just in case
+    # load new map w/ updated/new scanned region
+        print("req /map msg\n")
+        gmmsg_next = rospy.wait_for_message("/map", OccupancyGrid, timeout=None)
+        print("got /map msg\n")
+        gridmap = np.array(gmmsg_next.data).reshape((gmmsg_next.info.height, gmmsg_next.info.width))
+        explored_map = (gridmap != -1).astype(int)
+        explored_area_size = explored_map.sum() * gmmsg_next.info.resolution * gmmsg_next.info.resolution
+        reward = explored_area_size - previous_explored_area_size
+        reward = max(reward, 0) # This wont happen but just in case
 
-            print("/turtlebot3/odom msg\n")
-            odommsg = rospy.wait_for_message("/turtlebot3/odom", Odometry, timeout=None) # wanted to use /map to /base_link tfrom but tf.transform doesn't work here for some reason
-            print("got /turtlebot3/odom msg\n")
-            (wx, wy) = (odommsg.pose.pose.position.x, odommsg.pose.pose.position.y)
-            (gx, gy) = world2grid(gmmsg_next, wx, wy)
-        # save the map after moving the robot and the metadata
-            os.chdir(resdir)
-            imgname = 'map_after_moving%05d.pgm' % dataidx
-            savemap(imgname, gridmap, dataidx)
-            fname = 'metadata_after_moving%05d.dat' % dataidx
-            savemetadata(fname, dataidx, gx, gy, gx, gy, reward)
-            os.chdir(workdir)
+        print("reading tf to base_footprint \n")
+        #try:
+        # tfBuffer.wait
+        # trans = tfBuffer.lookup_transform('map', 'base_footprint', rospy.Time())
+        #except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+        #    continue
+        #    rate.sleep()
 
-        # update data
-            #previous_explored_area_size = explored_area_size
-            #(wx_prev, wy_prev) = (wx_next, wy_next)
-            #gmmsg_prev = gmmsg_next
-            dataidx = dataidx+1
+        t = rospy.Time(0)
+        listener.waitForTransform('map', 'base_link', t, rospy.Duration(0))
+        (trans, rot) = listener.lookupTransform("map", "base_link", t)
+
+        print("got the moved new tf msg (%f %f)\n"%  (trans[0], trans[1]) )#(trans.transform.translation.x, trans.transform.translation.y))
+        (wx, wy) = (trans[0], trans[1]) #(trans.transform.translation.x, trans.transform.translation.y)
+
+        # odommsg = rospy.wait_for_message("/odom", Odometry, timeout=None)
+        # (wx, wy) = (odommsg.pose.pose.position.x, odommsg.pose.pose.position.y)
+
+        (gx, gy) = world2grid(gmmsg_next, wx, wy)
+    # save the map after moving the robot and the metadata
+        #imgname = '%s/map_after_moving%05d.pgm' % (resdir, dataidx)
+        savemap(resdir, gridmap, dataidx)
+        fname = '%s/metadata_after_moving%05d.dat' % (resdir, dataidx)
+        savemetadata(fname, dataidx, gx, gy, gx, gy, reward)
 
     # reset map
-        reset_cmd = 'rosservice call /octomap_server/reset "{}" '
-        os.system(reset_cmd)
-        print("map is reset \n")
-        time.sleep(3.0)
+        if dataidx % 10 == 0:
+            print("reseting map \n")
+            #reset_gazebo()
+            #time.sleep(1.0)
+            reset_cmd = 'rosservice call /octomap_server/reset "{}" '
+            os.system(reset_cmd)
+            #reset_octomap()
+            time.sleep(0.5)
+            #listener.clear()
+    # end loop condition
+        if dataidx > MAX_NUM_DATA:
+            print("max num data reached.. finishing the data generation \n")
+            break
+
+        # update data
+        # previous_explored_area_size = explored_area_size
+        # (wx_prev, wy_prev) = (wx_next, wy_next)
+        # gmmsg_prev = gmmsg_next
+        dataidx = dataidx + 1
+
 # load comstmap
 
 
